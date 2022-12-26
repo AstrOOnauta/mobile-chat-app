27900ef9713a3f7e93da7c8fd8d64b5038bd656b
Will <willian.ralf.rn@gmail.com>
2022-12-26 14:59:45 -0300

    feat: set statusbar style

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import {StatusBar, useTheme} from 'native-base';
 import {NavigationContainer} from '@react-navigation/native';
 
 import AuthRoutes from './auth.routes';
 
 export default function Routes() {
+  const theme = useTheme();
+
   return (
     <NavigationContainer>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor={theme.colors.dark[100]}
+      />
       <AuthRoutes />
     </NavigationContainer>
   );
